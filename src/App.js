f98be374126a1c1ac3d718218780392d896b62eb
Yaara <yaara@tamcsolutions.info>
2022-12-12 19:46:41 +0200

    added sass but its not working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import './categories.styles.scss';
 
 const App = () => {
+  
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 1,
+      title: 'Jackets'
+    },
+    {
+      id: 1,
+      title: 'Sneakers'
+    },
+    {
+      id: 1,
+      title: 'Womens'
+    },
+    {
+      id: 1,
+      title: 'Mens'
+    }
+  ];
+
   return (
-    <div>
-      Hello world
-    </div>
+    <div className="categories-container">
+      {categories.map(({title, id}) => (
+        <div key={id} className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container"> 
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>   
   );
-}
+};
 
 export default App;
