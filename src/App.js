33d3c10c116438a970f7c3b8759d79ee10072d8e
Yaara <yaara@tamcsolutions.info>
2022-12-26 22:01:28 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import Directory from "./components/directory/directory.component";
-const App = () => {
-  
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png'
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
-    },
-  ];
+import { Routes, Route, Outlet} from "react-router-dom";
 
-  return <Directory categories={categories} />;
+import Home from "./routs/home/home.component";
+
+const App = () => {
+
+  const Navigation = () => {
+
+    return (
+      <div>
+        <div>
+          <h1>I am the navigation bar</h1>
+        </div>
+        <Outlet />
+      </div>
+    );
+  };
+  
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation/>}>
+        <Route index element={ <Home/>}/>
+        <Route path=''/>
+      </Route>
+    </Routes>
+  );
   
 };
 
