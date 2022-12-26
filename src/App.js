dcd680bed31dd7148adf9317c4cac239fd4f8b56
Yaara <yaara@tamcsolutions.info>
2022-12-26 20:29:15 +0200

    updating directory page woth directory component and category item component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,36 @@
-import './categories.styles.scss';
-
+import Directory from "./components/directory/directory.component";
 const App = () => {
   
   const categories = [
     {
       id: 1,
-      title: 'Hats'
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png'
     },
     {
-      id: 1,
-      title: 'Jackets'
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      id: 1,
-      title: 'Sneakers'
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      id: 1,
-      title: 'Womens'
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      id: 1,
-      title: 'Mens'
-    }
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
+    },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map(({title, id}) => (
-        <div key={id} className="category-container">
-          <div className="background-image" />
-          <div className="category-body-container"> 
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>   
-  );
+  return <Directory categories={categories} />;
+  
 };
 
 export default App;
